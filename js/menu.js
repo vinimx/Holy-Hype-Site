5baae56864f9d060ce8bfc3ebd9661c871b9d4f3
Marcos Vinícius Rocha <42097591+vinimx@users.noreply.github.com>
2025-04-24 22:55:46 -0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuBtn = document.getElementById('menu-btn');
+    const closeBtn = document.getElementById('close-menu');
+    const menu = document.getElementById('menu');
+    let isMenuOpen = false;
+
+    menuBtn.addEventListener('click', () => {
+        if (!isMenuOpen) {
+            menu.classList.remove('-right-full');
+            menu.classList.add('right-0');
+        } else {
+            menu.classList.remove('right-0');
+            menu.classList.add('-right-full');
+        }
+        isMenuOpen = !isMenuOpen;
+    });
+
+    closeBtn.addEventListener('click', () => {
+        menu.classList.remove('right-0');
+        menu.classList.add('-right-full');
+        isMenuOpen = false;
+    });
+
+    // Fechar menu ao clicar em um link
+    const menuLinks = menu.querySelectorAll('a');
+    menuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            if (window.innerWidth < 768) {
+                menu.classList.remove('right-0');
+                menu.classList.add('-right-full');
+                isMenuOpen = false;
+            }
+        });
+    });
+});
