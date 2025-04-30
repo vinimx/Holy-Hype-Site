fda47718926315074e2bcfc5eeb9a8ff218c127c
Marcos Vinícius Rocha <marcosvc817@gmail.com>
2025-04-29 23:28:16 -0300

    Adicionando todos os arquivos do projeto 2.0

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+function scrollToElement(elementId, event) {
+    event.preventDefault();
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
+}
